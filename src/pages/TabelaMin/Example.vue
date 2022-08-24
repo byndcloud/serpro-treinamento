5f0a64150f082b283eca4d42728dcd60b13492b2
Guilherme Lopes <gpl2@cin.ufpe.br>
2022-08-24 12:54:21 -0300

    fix: example Table

---
+++ src/pages/TabelaMin/Example.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Tela de Preenchimento de Dados</h1>
+    <v-text-field outlined filled></v-text-field>
+    <qt-table-list listAction="users/search" :fields="fields"></qt-table-list>
+    <v-btn color="secondary">Enviar formulário</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fields: {
+        name: 'Nome',
+        cpf: 'CPF',
+        user_profile_id: 'Perfil',
+        registered: 'Status',
+        action: 'Ações'
+      }
+    }
+  }
+}
+</script>

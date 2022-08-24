0e83568007ff8d1463bd44f804088619d66ce6ff
Guilherme Lopes <gpl2@cin.ufpe.br>
2022-08-24 12:04:12 -0300

    fix: prettier eslint working

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  plugins: ["vuetify"],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": "warn",
+    "vuetify/no-deprecated-classes": "error",
+    "vuetify/grid-unknown-attributes": "error",
+    "vuetify/no-legacy-grid": "error",
+  },
+};
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,25 @@
+const path = require('path')
+
 module.exports = {
   productionSourceMap: false,
   css: {
     extract: false,
+    loaderOptions: {
+      // TODO: this should be the ideal, but vue.config is not working
+      sass: {
+        additionalData: `
+          @import "~vuetify/src/styles/styles.sass"
+          @import "@/styles/index.sass"
+          @import "@/styles/variables.sass"
+        `
+      }
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src/')
+      }
+    }
   },
 };
